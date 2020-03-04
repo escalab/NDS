84d262e31e6b319fed1aa9126ef459f96b3e7f83
hankliu5 <yliu719@ucr.edu>
2020-03-03 18:29:16 -0800

    create normal format and block format at the same time

--- datagen.c
+++ datagen.c
@@ -3,24 +3,56 @@
 #include <fcntl.h>
 
 int main(int argc, char** argv) {
-    int i, n;
+    int i, j, ii, jj, n, sub_n;
     FILE *fptr;
-    double tmp;
-
-    if (argc < 3) {
-        printf("usage: %s <output path> <matrix size>\n", argv[0]);
+    long **output_matrix;
+    long *submatrix;
+    int idx;
+    
+    if (argc < 4) {
+        printf("usage: %s <normal output path> <block output path> <matrix size> <submatrix size>\n", argv[0]);
         return 1;
     }
 
-    srand(5);
+    fptr = fopen(argv[1], "w");
+    n = atoi(argv[3]);
+    sub_n = atoi(argv[4]);
 
-    fptr = fopen(argv[1], "w+b");
-    n = atoi(argv[2]);
-    for (i = 0; i < n * n; i++) {
-        tmp = (double) rand() / RAND_MAX;
-        fwrite(&tmp, sizeof(double), 1, fptr);
+    output_matrix = (long **) malloc(n * sizeof(long *));
+    submatrix = (long *) malloc(sub_n * sub_n * sizeof(long));
+    
+    for (i = 0; i < n; i++) {
+        output_matrix[i] = (long *) malloc(n * sizeof(long));
+        for (j = 0; j < n; j++) {
+            output_matrix[i][j] = (long) i * n + j;
+            fwrite(&(output_matrix[i][j]), sizeof(long), 1, fptr);
+        }
     }
 
     fclose(fptr);
+
+    fptr = fopen(argv[2], "w");
+
+    for(i = 0; i < n; i+= sub_n) {
+        for(j = 0; j < n; j+= sub_n) {  
+            idx = 0;     
+            for(ii = i; ii < i+sub_n; ii++) {
+                for(jj = j; jj < j+sub_n; jj++) {
+                    submatrix[idx] = output_matrix[ii][jj];
+                    idx++;
+                }
+            }
+            fwrite(submatrix, sizeof(long), sub_n * sub_n, fptr);
+        }
+    }  
+
+    fclose(fptr);
+    for (i = 0; i < n; i++) {
+        free(output_matrix[i]);
+    }    
+    free(output_matrix);
+
+    free(submatrix);
+
     return 0;
 }
