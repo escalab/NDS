fe7c646d26a8a09d736d893c82ef521231124b48
hankliu5 <yliu719@ucr.edu>
2020-03-04 11:23:24 -0800

    add timing for deserialization

--- read_sequential.c
+++ read_sequential.c
@@ -7,7 +7,8 @@ int main(int argc, char** argv) {
     int i, j, ii, jj, sub_i, offset, n, sub_n;
     FILE *fptr;
     long *sub_matrix;
-    
+    long duration;
+    struct timeval h_start, h_end;
     if (argc < 3) {
         printf("usage: %s <path> <matrix size> <submatrix size>\n", argv[0]);
         return 1;
@@ -19,6 +20,7 @@ int main(int argc, char** argv) {
 
     sub_matrix = (long*) malloc(sub_n * sub_n * sizeof(long));
 
+    gettimeofday(&h_start, NULL);
     for (i = 0; i < n; i+= sub_n) {
         for (j = 0; j < n; j+= sub_n) {           
             sub_i = 0;        
@@ -29,6 +31,7 @@ int main(int argc, char** argv) {
                 sub_i++;
             }
 
+        #ifdef DEBUG
             // debug purpose
             for(ii = 0; ii < sub_n; ii++) {
                 for (jj = 0; jj < sub_n; jj++) {
@@ -37,10 +40,15 @@ int main(int argc, char** argv) {
                 printf("\n");
             }
             printf("\n");
+        #endif
         }
     }
+    gettimeofday(&h_end, NULL);
+    duration = ((h_end.tv_sec - h_start.tv_sec) * 1000000) + (h_end.tv_usec - h_start.tv_usec);
 
     free(sub_matrix);
     fclose(fptr);
+    printf("duration: %f s\n", (float) duration / 1000000);
+
     return 0;
 }
