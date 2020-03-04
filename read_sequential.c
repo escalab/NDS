19ddd7380a2446640bf1caa3ba96f2c8b1be46a1
hankliu5 <yliu719@ucr.edu>
2020-03-04 11:01:45 -0800

    submatrix should be in an array format

--- read_sequential.c
+++ read_sequential.c
@@ -6,7 +6,7 @@
 int main(int argc, char** argv) {
     int i, j, ii, jj, sub_i, offset, n, sub_n;
     FILE *fptr;
-    long **sub_matrix;
+    long *sub_matrix;
     
     if (argc < 3) {
         printf("usage: %s <path> <matrix size> <submatrix size>\n", argv[0]);
@@ -17,11 +17,7 @@ int main(int argc, char** argv) {
     n = atoi(argv[2]);
     sub_n = atoi(argv[3]);
 
-    sub_matrix = (long**) malloc(sub_n * sizeof(long*));
-    
-    for (i = 0; i < sub_n; i++) {
-        sub_matrix[i] = (long *) malloc(sub_n * sizeof(long));
-    }
+    sub_matrix = (long*) malloc(sub_n * sub_n * sizeof(long));
 
     for (i = 0; i < n; i+= sub_n) {
         for (j = 0; j < n; j+= sub_n) {           
@@ -29,23 +25,19 @@ int main(int argc, char** argv) {
             for(ii = i; ii < i+sub_n; ii++) {
                 offset = (ii * n + j) * sizeof(long);
                 fseek(fptr, offset, SEEK_SET);
-                fread(sub_matrix[sub_i], sizeof(long), sub_n, fptr);
+                fread((sub_matrix + sub_i * sub_n), sizeof(long), sub_n, fptr);
                 sub_i++;
             }
 
             // debug purpose
             for(ii = 0; ii < sub_n; ii++) {
                 for (jj = 0; jj < sub_n; jj++) {
-                    printf("%ld ", sub_matrix[ii][jj]);
+                    printf("%ld ", sub_matrix[ii * sub_n + jj]);
                 }
                 printf("\n");
             }
             printf("\n");
         }
-    }
-
-    for (i = 0; i < sub_n; i++) {
-        free(sub_matrix[i]);
     }
 
     free(sub_matrix);
