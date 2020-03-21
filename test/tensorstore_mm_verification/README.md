5e19fd0738b2a51410c115b31f00adaeffbc1aaa
hankliu5 <yliu719@ucr.edu>
2020-03-20 18:22:03 -0700

    verify different tensorstore_mm algorithms

---
+++ test/tensorstore_mm_verification/README.md
@@ -0,0 +1,3 @@
+# TensorStore MM Verification
+
+Using wholeMatrix_Dgemm to verify the result of customized formats and algorithm, which use cuBLAS library, are correct.
--- test/tensorstore_mm_verification/verify.cu
+++ test/tensorstore_mm_verification/verify.cu
@@ -10,28 +10,16 @@
 // for timing
 #include <sys/time.h>
 
-// this one is good because it only takes one element space
-int cpu_verify(double *A, double *B, float *C, unsigned int m, unsigned int n, unsigned int k) {
-    const float relativeTolerance = 1e-3;
-  
-    for(int row = 0; row < m; ++row) {
-        for(int col = 0; col < n; ++col) {
-            float sum = 0;
-            for(unsigned int i = 0; i < k; ++i) {
-                // printf("C[%u][%u] = A[%u][%u] * B[%u][%u]\n", row, col, row, i, i, col);
-                sum += (float) A[row*k + i]*B[i*n + col];
-            }
-            float relativeError = (sum - C[row*n + col])/sum;
-            if (fabs(relativeError) > relativeTolerance) {
-                printf("(%d, %d) = %f, supposed to be %f\n", row, col, C[row*n + col], sum); 
-                printf("TEST FAILED\n\n");
-                return 0;
-            }
-        }
-    }
-    printf("TEST PASSED\n\n");
-    return 1;
-}
+// ALGO 0: wholeMatrix_Sgemm
+// ALGO 1: wholeMatrix_Sgemm_half
+// ALGO 2: sequential_blockSgemm
+// ALGO 3: sequential_blockSgemm_half
+// ALGO 4: tensor_blockSgemm
+// ALGO 5: tensor_blockSgemm_half
+
+#ifndef ALGO
+#define ALGO 0
+#endif
 
 int verify(const float *C, const float *answer, int m, int n) {
     const float relativeTolerance = 1e-3;
@@ -51,40 +39,61 @@ int verify(const float *C, const float *answer, int m, int n) {
     return 1;
 }
 
-
 int main(int argc, char** argv) {
     double *a, *b;
-    double *a_tensor, *b_tensor;
-    float *c, *c_reformat, *answer_c;
-    int n, sub_n, need_output = 0, is_passed = 0;
+    float *c, *answer_c;
+    int n, need_output = 0, is_passed = 0;
     int a_fd, b_fd;
-    int a_tensor_fd, b_tensor_fd;
     struct timeval h_start, h_end;
     long duration;
 
+#if ALGO >= 4
+    int sub_n;
+    int a_tensor_fd, b_tensor_fd;
+    double *a_tensor, *b_tensor;
+    float *c_reformat;
     if (argc < 5) {
-        printf("usage: %s <sequence format path> <tensor format path> <matrix size> <submatrix size> [output?]\n", argv[0]);
+        printf("usage: %s <seq_matrix path> <tensor_matrix path> <matrix size> <submatrix size> [output?]\n", argv[0]);
         exit(1);
     }
-
-    // GEMM configuration.
-    a_fd = open(argv[1], O_RDONLY);
-    b_fd = open(argv[1], O_RDONLY);
     a_tensor_fd = open(argv[2], O_RDONLY);
     b_tensor_fd = open(argv[2], O_RDONLY);
-
     n = atoi(argv[3]);
     sub_n = atoi(argv[4]);
-
     if (argc > 5) {
         need_output = atoi(argv[5]);
     }
-
-    a = (double *) mmap(NULL, sizeof(double) * n * n, PROT_READ, MAP_PRIVATE, a_fd, 0);
-    b = (double *) mmap(NULL, sizeof(double) * n * n, PROT_READ, MAP_PRIVATE, b_fd, 0);
     a_tensor = (double *) mmap(NULL, sizeof(double) * n * n, PROT_READ, MAP_PRIVATE, a_tensor_fd, 0);
     b_tensor = (double *) mmap(NULL, sizeof(double) * n * n, PROT_READ, MAP_PRIVATE, b_tensor_fd, 0);
     c_reformat = (float *) calloc(n * n, sizeof(float));
+
+#elif ALGO >= 2
+    int sub_n;
+    if (argc < 4) {
+        printf("usage: %s <matrix path> <matrix size> <submatrix size> [output?]\n", argv[0]);
+        exit(1);
+    }
+    n = atoi(argv[2]);
+    sub_n = atoi(argv[3]);
+    if (argc > 4) {
+        need_output = atoi(argv[4]);
+    }
+#else
+    if (argc < 3) {
+        printf("usage: %s <matrix path> <matrix size> [output?]\n", argv[0]);
+        exit(1);
+    }
+    n = atoi(argv[2]);
+    if (argc > 3) {
+        need_output = atoi(argv[3]);
+    }
+#endif
+    // GEMM configuration.
+    a_fd = open(argv[1], O_RDONLY);
+    b_fd = open(argv[1], O_RDONLY);
+
+    a = (double *) mmap(NULL, sizeof(double) * n * n, PROT_READ, MAP_PRIVATE, a_fd, 0);
+    b = (double *) mmap(NULL, sizeof(double) * n * n, PROT_READ, MAP_PRIVATE, b_fd, 0);
 
     c = (float *) calloc(n * n, sizeof(float));
     answer_c = (float *) calloc(n * n, sizeof(float));
@@ -100,10 +109,60 @@ int main(int argc, char** argv) {
     printf("calculating the result of the sequential format\n");
     memset(c, 0, n * n * sizeof(float));
     gettimeofday(&h_start, NULL);
+#if ALGO == 0
+    wholeMatrix_Sgemm(n, n, n, a, b, c);
+#elif ALGO == 1
+    wholeMatrix_Sgemm_half(n, n, n, a, b, c);
+#elif ALGO == 2
+    sequential_blockSgemm(n, n, n, sub_n, sub_n, sub_n, a, b, c);
+#elif ALGO == 3
     sequential_blockSgemm_half(n, n, n, sub_n, sub_n, sub_n, a, b, c);
+#elif ALGO == 4
+    tensor_blockSgemm(n, n, n, sub_n, sub_n, sub_n, a_tensor, b_tensor, c);
+#elif ALGO == 5
+    tensor_blockSgemm_half(n, n, n, sub_n, sub_n, sub_n, a_tensor, b_tensor, c);
+#endif
     gettimeofday(&h_end, NULL);
     duration = ((h_end.tv_sec - h_start.tv_sec) * 1000000) + (h_end.tv_usec - h_start.tv_usec);
-    printf("sequential format block-GEMM duration: %f ms\n", (float) duration / 1000);    
+    printf("GEMM duration: %f ms\n", (float) duration / 1000);    
+
+#if ALGO >= 4
+    printf("Reformat from tensor to sequential...\n");
+    int count = 0;
+    gettimeofday(&h_start, NULL);
+
+    for (int i = 0; i < n; i += sub_n) {
+        for (int j = 0; j < n; j += sub_n) {  
+            for(int ii = i; ii < i + sub_n; ii++) {
+                for(int jj = j; jj < j + sub_n; jj++) {
+                    // printf("ii: %d, jj: %d\n", ii, jj);
+                    c_reformat[ii * n + jj] = c[count];
+                    count++;
+                }
+            }
+        }
+    }
+    gettimeofday(&h_end, NULL);
+    duration = ((h_end.tv_sec - h_start.tv_sec) * 1000000) + (h_end.tv_usec - h_start.tv_usec);
+    printf("Reformat from tensor to sequential duration: %f ms\n", (float) duration / 1000);  
+    is_passed = verify(c_reformat, answer_c, n, n);
+#else
+    is_passed = verify(c, answer_c, n, n);
+#endif
+
+    if (is_passed && need_output) {
+        char filename[64];
+        FILE *fptr;
+#if ALGO >= 4
+        printf("writing tensor format answer to %s\n", filename);
+        sprintf(filename, "ans_block_%d_%d.bin", n, sub_n);
+#else
+        printf("writing sequential answer to %s\n", filename);
+        sprintf(filename, "ans_%d.bin", n);
+#endif
+        fptr = fopen(filename, "wb");
+        fwrite(c, sizeof(float), n * n, fptr);
+    }
 
 #ifdef DEBUG
     int i, j;
@@ -132,92 +191,21 @@ int main(int argc, char** argv) {
     printf("\n");
 #endif
 
-    is_passed = verify(c, answer_c, n, n);
-    if (is_passed && need_output) {
-        char filename[64];
-        FILE *fptr;
-        sprintf(filename, "ans_%d.bin", n);
-        fptr = fopen(filename, "wb");
-        printf("writing sequential answer to %s\n", filename);
-        fwrite(c, sizeof(float), n * n, fptr);
-    }
-
-    // GEMM configuration.
-    printf("calculating the result of the tensor format\n");
-    memset(c, 0, n * n * sizeof(float));
-    gettimeofday(&h_start, NULL);
-    tensor_blockSgemm_half(n, n, n, sub_n, sub_n, sub_n, a_tensor, b_tensor, c);
-    gettimeofday(&h_end, NULL);
-    duration = ((h_end.tv_sec - h_start.tv_sec) * 1000000) + (h_end.tv_usec - h_start.tv_usec);
-    printf("tensor format block-GEMM duration: %f ms\n", (float) duration / 1000);
-    
-    printf("Reformat from tensor to sequential...\n");
-    int count = 0;
-    gettimeofday(&h_start, NULL);
-
-    for (int i = 0; i < n; i += sub_n) {
-        for (int j = 0; j < n; j += sub_n) {  
-            for(int ii = i; ii < i + sub_n; ii++) {
-                for(int jj = j; jj < j + sub_n; jj++) {
-                    // printf("ii: %d, jj: %d\n", ii, jj);
-                    c_reformat[ii * n + jj] = c[count];
-                    count++;
-                }
-            }
-        }
-    }
-
-    gettimeofday(&h_end, NULL);
-    duration = ((h_end.tv_sec - h_start.tv_sec) * 1000000) + (h_end.tv_usec - h_start.tv_usec);
-    printf("Reformat from tensor to sequential duration: %f ms\n", (float) duration / 1000);  
-#ifdef DEBUG
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            printf("%f ", a_tensor[i * n + j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            printf("%f ", b_tensor[i * n + j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            printf("%f ", c[i * n + j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-#endif
-
-    is_passed = verify(c_reformat, answer_c, n, n);
-    if (is_passed && need_output) {
-        char filename[64];
-        FILE *fptr;
-        sprintf(filename, "ans_block_%d_%d.bin", n, sub_n);
-        fptr = fopen(filename, "wb");
-        printf("writing tensor format answer to %s\n", filename);
-        fwrite(c, sizeof(float), n * n, fptr);
-    }
-
     munmap(a, sizeof(double) * n * n);
     munmap(b, sizeof(double) * n * n);
     close(a_fd);
     close(b_fd);
 
+    free(answer_c);
+    free(c);
+
+#if ALGO >= 4
     munmap(a_tensor, sizeof(double) * n * n);
     munmap(b_tensor, sizeof(double) * n * n);
     close(a_tensor_fd);
     close(b_tensor_fd);
-
-    free(answer_c);
     free(c_reformat);
-    free(c);
+#endif
+
     return 0;
 }
