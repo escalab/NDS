430314f0009d6f70e30faaa659aab5a44069365f
hankliu5 <yliu719@ucr.edu>
2020-05-21 18:27:18 -0700

    use heap memory to avoid seqfault in stack

--- lib/tensorstore/tensorstore.c
+++ lib/tensorstore/tensorstore.c
@@ -13,9 +13,10 @@ void extract_submatrix_from_seq(double *seq_matrix, double *sub_matrix, size_t n
 
 void tensor_matrix_transpose(double *tensor_matrix, size_t n_row, size_t n_col, size_t sub_m, size_t sub_n) {
     size_t i, j;
-    double temp[sub_m*sub_n];
     size_t cross_row = sub_m * n_col, cross_col = sub_m * sub_n;
-    double *upper_ptr, *lower_ptr;
+    double *temp, *upper_ptr, *lower_ptr;
+
+    temp = malloc(sizeof(double) * sub_m * sub_n);
     // iterate submatrix at upper triangles
     // for example 4 * 4 matrix: 0, 1, 2, 3 
     for (i = 0; i < n_row / sub_m; i++) {
@@ -25,17 +26,21 @@ void tensor_matrix_transpose(double *tensor_matrix, size_t n_row, size_t n_col, 
             upper_ptr = tensor_matrix + i * cross_row + j * cross_col;
             // 1, 0 -> 4
             lower_ptr = tensor_matrix + j * cross_row + i * cross_col; 
-            memcpy(&temp, upper_ptr, sizeof(double) * sub_m * sub_n);
+            memcpy(temp, upper_ptr, sizeof(double) * sub_m * sub_n);
             memcpy(upper_ptr, lower_ptr, sizeof(double) * sub_m * sub_n);
-            memcpy(lower_ptr, &temp, sizeof(double) * sub_m * sub_n);
+            memcpy(lower_ptr, temp, sizeof(double) * sub_m * sub_n);
         }
     }
+
+    free(temp);
 }
 
 void seq_matrix_transpose(double *seq_matrix, size_t n_row, size_t n_col, size_t sub_m, size_t sub_n) {
     size_t i, j, ii;
-    double temp[sub_n];
-    double *upper_ptr, *lower_ptr;
+    double *temp, *upper_ptr, *lower_ptr;
+
+    temp = malloc(sizeof(double) * sub_n);
+
     // iterate submatrix at upper triangles 
     for (i = 0; i < n_row / sub_m; i++) {
         for (j = i+1; j < n_col / sub_n; j++) {
@@ -44,12 +49,14 @@ void seq_matrix_transpose(double *seq_matrix, size_t n_row, size_t n_col, size_t
             // iterate rows in the submatrix
             for (ii = 0; ii < sub_m; ii++) {
                 // swap elements in a row
-                memcpy(&temp, upper_ptr + ii * n_col, sizeof(double) * sub_n);
+                memcpy(temp, upper_ptr + ii * n_col, sizeof(double) * sub_n);
                 memcpy(upper_ptr + ii * n_col, lower_ptr + ii * n_col, sizeof(double) * sub_n);
-                memcpy(lower_ptr + ii * n_col, &temp, sizeof(double) * sub_n);
+                memcpy(lower_ptr + ii * n_col, temp, sizeof(double) * sub_n);
             }
         }
     }
+
+    free(temp);
 }
 
 void seq2tensor(double *seq_matrix, double *tensor_matrix, size_t m, size_t n, size_t sub_m, size_t sub_n) {
