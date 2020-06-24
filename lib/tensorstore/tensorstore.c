6d8e7d2bae0407fd0d6d28308ab266f3ec683748
hankliu5 <yliu719@ucr.edu>
2020-06-24 11:44:11 -0700

    transform functions use memcpy & move test to test folder

--- lib/tensorstore/tensorstore.c
+++ lib/tensorstore/tensorstore.c
@@ -1,16 +1,5 @@
 #include "tensorstore.h"
 
-void extract_submatrix_from_seq(double *seq_matrix, double *sub_matrix, size_t n_col, size_t sub_m, size_t sub_n) {
-    size_t ii, jj, idx;
-    idx = 0;     
-    for(ii = 0; ii < sub_m; ii++) {
-        for(jj = 0; jj < sub_n; jj++) {
-            sub_matrix[idx] = seq_matrix[ii * n_col + jj];
-            idx++;
-        }
-    }
-}
-
 void tensor_matrix_transpose(double *tensor_matrix, double *out_matrix, size_t n_row, size_t n_col, size_t sub_m, size_t sub_n) {
     size_t i, j;
     size_t cross_row = sub_m * n_col, cross_col = sub_m * sub_n;
@@ -58,24 +47,27 @@ void seq_matrix_transpose(double *seq_matrix, double *out_matrix, size_t n_row, 
 }
 
 void seq2tensor(double *seq_matrix, double *tensor_matrix, size_t m, size_t n, size_t sub_m, size_t sub_n) {
-    size_t i, j;
+    size_t i, j, ii;
     size_t cross_row = m, cross_col = sub_m;
-    for(i = 0; i < m; i+= sub_m) {
-        for(j = 0; j < n; j+= sub_n) {
-            extract_submatrix_from_seq((seq_matrix + i * n + j), (tensor_matrix + i * cross_row + j * cross_col), n, sub_m, sub_n);
+    double *src, *dst;
+    for (i = 0; i < m; i += sub_m) {
+        for (j = 0; j < n; j += sub_n) {
+            dst = tensor_matrix + i * cross_row + j * cross_col;
+            src = seq_matrix + i * n + j;
+            for (ii = 0; ii < sub_m; ii++) {
+                memcpy((dst + ii * sub_n), (src + ii * n), sizeof(double) * sub_n);
+            }
         }
     }  
 }
 
 void tensor2seq(double *tensor_matrix, double *seq_matrix, size_t m, size_t n, size_t sub_m, size_t sub_n) {
-    size_t i, j, ii, jj, count = 0;
-    for (i = 0; i < n; i += sub_n) {
-        for (j = 0; j < n; j += sub_n) {  
-            for(ii = i; ii < i + sub_n; ii++) {
-                for(jj = j; jj < j + sub_n; jj++) {
-                    seq_matrix[ii * n + jj] = tensor_matrix[count];
-                    count++;
-                }
+    size_t i, j, ii, idx = 0;
+    for (i = 0; i < n; i += sub_m) {
+        for (j = 0; j < n; j += sub_n) {
+            for(ii = i; ii < i + sub_m; ii++) {
+                memcpy((seq_matrix + ii * n + j), (tensor_matrix + idx), sizeof(double) * sub_n);
+                idx += sub_n;
             }
         }
     }
